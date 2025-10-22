04226d333cf76ff581e2dbd28157ed2a994d12c0
Carlos <carlos@local.es>
2025-10-22 20:18:36 +0200

    Restauración del viejo sistemas por SolicitanteService

--- src/main/java/dam/proy/ticketing/app/services/interfaces/ISolicitanteService.java
+++ src/main/java/dam/proy/ticketing/app/services/interfaces/ISolicitanteService.java
@@ -2,7 +2,21 @@ package dam.proy.ticketing.app.services.interfaces;
 
 
 import dam.proy.ticketing.app.models.Solicitante;
+import dam.proy.ticketing.app.models.Ticket;
+import dam.proy.ticketing.app.models.dto.TicketCreacionDTO;
+
+import java.util.List;
 
 public interface ISolicitanteService {
     Solicitante nuevoSolicitante(Solicitante solicitante);
+
+    /**
+     * Define el método para obtener tickets.
+     */
+    List<Ticket> obtenerTicketsPorEmail(String email);
+
+    /**
+     * Define el método para crear un ticket.
+     */
+    Ticket crearTicket(TicketCreacionDTO ticketDTO, String email);
 }
